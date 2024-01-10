1a1c16ade8c67f1c412c57a95ce785147c1655cf
Michael Cheng <cheng.c.mike@gmail.com>
2024-01-10 12:04:01 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
 import { getTrivia, addOneToCompletedCount } from "@/lib/trivia-db";
 
 export default async function Home() {
-  const { trivia } = await getTrivia();
-  console.log("=====", trivia);
-
-  async function action(data: FormData) {
-    "use server";
-
-    const title = data.get("title");
-    if (!title || typeof title !== "string") {
-      return;
-    }
-
-    // call server action
-    await addOneToCompletedCount();
-  }
-
-  return (
-    <div>
-      <button onClick={action}>ADD</button>
-    </div>
-  );
+  return <div>HIHI</div>;
 }
