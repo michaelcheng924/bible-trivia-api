5cf49c057125ba283c4f7d300d17be7c657c2f38
Michael Cheng <cheng.c.mike@gmail.com>
2024-01-10 13:42:30 -0600

    update cache control

--- src/app/api/trivia/route.ts
+++ src/app/api/trivia/route.ts
@@ -9,6 +9,7 @@ export async function GET() {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Cache-Control": "no-store",
     },
   });
 }
